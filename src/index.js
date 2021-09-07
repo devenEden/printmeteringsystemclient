053c8625fee4711e920fed8cf2b2b0b33db25688
feta_deven <deshdeven@gmail.com>
2021-09-07 03:57:46 -0700

    moved position of index css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 import "antd/dist/antd.css";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";

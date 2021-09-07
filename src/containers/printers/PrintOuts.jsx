a99ae5448665bbcb03b43401dfe1824fc9e80e1d
feta_deven <deshdeven@gmail.com>
2021-09-07 16:34:34 -0700

    added all app pages

---
+++ src/containers/printers/PrintOuts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrintOuts = () => {
+  return (
+    <div>
+      <h1>Printers</h1>
+    </div>
+  );
+};
+
+export default PrintOuts;
---
+++ src/containers/printers/Printers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Printers = () => {
+  return (
+    <div id="main-container">
+      <h1>Printers</h1>
+    </div>
+  );
+};
+
+export default Printers;

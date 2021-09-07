d8d64f8516fae1d396d653510802286e8d4afb0b
feta_deven <deshdeven@gmail.com>
2021-09-07 03:57:24 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import routes from "./config/routes/routes";
+import PageNotFound from "./containers/Shared/App/404";
+import MainLayout from "./containers/Shared/MainLayout";
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path={routes.authentication.login.path}
+            component={routes.authentication.login.component}
+          />
+          <Route
+            exact
+            path={routes.authentication.resetPassword.path}
+            component={routes.authentication.resetPassword.component}
+          />
+          <Route
+            exact
+            path={routes.authentication.confirmAccount.path}
+            component={routes.authentication.confirmAccount.component}
+          />
+          <Route path="/" component={MainLayout} />
+          <Route>
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
